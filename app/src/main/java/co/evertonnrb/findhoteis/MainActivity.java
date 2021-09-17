fe31a15e42f79dd34e3cbf283a34143fb571c436
Everton Ribeiro <everton@debian.everton>
2021-09-16 22:47:13 -0400

    drawer

--- app/src/main/java/co/evertonnrb/findhoteis/MainActivity.java
+++ app/src/main/java/co/evertonnrb/findhoteis/MainActivity.java
@@ -1,14 +1,86 @@
 package co.evertonnrb.findhoteis;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private int mOpcaoSelecionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                toolbar,
+                R.string.app_name,
+                R.string.app_name
+        );
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+
+        mNavigationView = (NavigationView)findViewById(R.id.navigationView);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selecionarOpcaoMenu(item);
+                return true;
+            }
+        });
+        if (savedInstanceState == null){
+            mOpcaoSelecionada = R.id.menu_action_aba;
+        }else{
+            mOpcaoSelecionada = savedInstanceState.getInt("menuItem");
+        }
+        selecionarOpcaoMenu(mNavigationView.getMenu().findItem(mOpcaoSelecionada));
+    }
+
+    private void selecionarOpcaoMenu(MenuItem item) {
+        mOpcaoSelecionada = item.getItemId();
+        item.setChecked(true);
+        mDrawerLayout.closeDrawers();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putInt("menuItem",mOpcaoSelecionada);
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
